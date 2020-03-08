9b05ca5a06d2cf82dcaae1e9d5ea663c2153d716
Adriano Praia <adrianopraia@live.com>
2020-03-08 19:14:41 -0300

    Adicionado RazorExtensions

---
+++ Thomas.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Thomas.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataStatus(this RazorPage page, int tipoStatus)
+        {
+            if (tipoStatus == 1) return "Aberto";
+
+            if (tipoStatus == 2) return "Fechado";
+
+            if (tipoStatus == 3) return "Pausado";
+
+            return "Cancelado";
+        }
+    }
+}
