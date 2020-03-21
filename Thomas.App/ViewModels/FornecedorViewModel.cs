d21759f37d33508e2f1fc7fce1b0270dbe6b2cb3
Adriano Praia <adrianopraia@live.com>
2020-03-21 19:00:04 -0300

    Ajustado FornecedorViewModel

--- Thomas.App/ViewModels/FornecedorViewModel.cs
+++ Thomas.App/ViewModels/FornecedorViewModel.cs
@@ -18,8 +18,15 @@ namespace Thomas.App.ViewModels
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Contato { get; set; }
+
+        [DisplayName("Tipo")]
+        public int TipoFornecedor { get; set; }
 
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
